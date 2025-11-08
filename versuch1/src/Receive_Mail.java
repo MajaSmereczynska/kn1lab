e07a8caafe8b55902dc4d3dd1df4d47fa8fa0426
Maja Smereczynska <83644808+MajaSmereczynska@users.noreply.github.com>
2025-11-08 11:56:49 +0100

    exercise 1.3
    Added functionality to fetch and display emails from a POP3 server, including connection setup, message retrieval, and printing details.

--- versuch1/src/Receive_Mail.java
+++ versuch1/src/Receive_Mail.java
@@ -10,9 +10,79 @@ public class Receive_Mail {
 	}
 	
 	public static void fetchMail() {
+		// We declare Store and Folder here as null.
+		// This is important so we can access them in the "finally" block
+		// to close them, even if an error happens inside the "try" block
+		Store store = null;
+		Folder emailFolder = null;
+		
 		try {
-			// your code here
-		} catch(Exception e) {
+			// Define Connection Parameters
+			String host = "localhost";
+			String user = "labrat";
+			String password = "kn1lab";
+			String storeType = "pop3";
+
+            // Set system properties for the session
+            // We tell the Java Mail API where to find the POP3 (receiving) server
+            Properties properties = System.getProperties();
+            properties.setProperty("mail.pop3.host", host);
+
+			// Get a Mail Session
+			// The Session object coordinates all mail activities
+			Session session = Session.getDefaultInstance(properties);
+			
+			// Connect to the Store
+			// A "Store" is the service that stores emails (like a POP3 server)
+			store = session.getStore(storeType);
+			System.out.println("Connecting to POP3 server on " + host + "...");
+			
+			// This is the actual login.
+			store.connect(host, user, password);
+			System.out.println("Connection successful!");
+
+			// Open the INBOX Folder
+			// A "Folder" is where messages are held. For POP3, it's always "INBOX"
+			emailFolder = store.getFolder("INBOX");
+			
+			// We open the folder in READ_ONLY mode because we just want to look, not make changes
+			emailFolder.open(Folder.READ_ONLY);
+
+			// Fetch and Loop Through Messages
+			// Get all messages from the folder and put them in an array
+			Message[] messages = emailFolder.getMessages();
+			System.out.println("Emails in INBOX: " + messages.length);
+			System.out.println("----------------------------------------\n");
+
+			// Loop through every message in the array.
+			for (int i = 0; i < messages.length; i++) {
+				Message message = messages[i];
+				
+				// Print Details (as required by the assignment)
+				
+				// Requirement: Numbering
+				System.out.println("--- EMAIL #" + (i + 1) + " ---");
+				
+				// Requirement: Sender
+				if (message.getFrom() != null && message.getFrom().length > 0) {
+					System.out.println("From: " + message.getFrom()[0].toString());
+				}
+				
+				// Requirement: Subject
+				System.out.println("Subject: " + message.getSubject());
+				
+				// Requirement: Sent Date
+				System.out.println("Sent Date: " + message.getSentDate());
+				
+				// Requirement: Content
+				System.out.println("Content: " + message.getContent().toString());
+				System.out.println("\n");
+			}
+
+            store.close();
+
+		} catch (Exception e) {
+			// If anything goes wrong (bad password, server down), print the error.
 			e.printStackTrace();
 		}
 	}
